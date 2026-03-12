daee04794ad64185d9b42fb5d27f6d6ff07e5d29
Hayk-Tumasyan <hayk.tumasyan2004@gmail.com>
2026-03-12 19:28:11 +0400

    Securing API keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIXZLZYegRadJFxvZYYWgNq-h5xCSkSbw",
-  authDomain: "the-round-table-f1d77.firebaseapp.com",
-  projectId: "the-round-table-f1d77",
-  storageBucket: "the-round-table-f1d77.firebasestorage.app",
-  messagingSenderId: "438208622266",
-  appId: "1:438208622266:web:0c0517ba587792147bdc83"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
